7d6721c2c502c48001551163167010304aac2ca6
bavana <bavana.sachidanand@bjss.com>
2021-11-26 14:25:01 +0000

    able to chat with other users

--- socket-io-server/src/index.js
+++ socket-io-server/src/index.js
@@ -5,15 +5,20 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  return res.status(200).send("Hello from the backend")
 });
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
-  });
+// io.on('connection', (socket) => {
+//     socket.on('chat message', (msg) => {
+//         io.emit('chat message', msg);
+//     });
+//   });
 
 server.listen(3001, () => {
   console.log('listening on *:3001');
