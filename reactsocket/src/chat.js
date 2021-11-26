05be3ada330e0dfcf1bcff97944377ca55dd2e6c
rachael.slack <rslack@LPT6368.bjss.co.uk>
2021-11-26 00:34:15 +0000

    new chat app function added

---
+++ reactsocket/src/chat.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Chat(){
+    return(
+        <div className="App">
+            <p> this will be the chat window div!</p>
+        </div>
+    )
+}
+
+export default Chat;
--- reactsocket/src/index.js
+++ reactsocket/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Chat from './chat.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Chat />
   </React.StrictMode>,
   document.getElementById('root')
 );
