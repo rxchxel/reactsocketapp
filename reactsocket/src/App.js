cf869c5eb68bf0feaa7221b60c69a34db117a444
rachael.slack <rslack@LPT6368.bjss.co.uk>
2021-11-26 05:08:04 +0000

    push changes

--- reactsocket/src/App.js
+++ reactsocket/src/App.js
@@ -1,24 +1,31 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import { io } from "socket.io-client";
-import Messages from './Messages';
+//import Messages from './Messages';
 import MessageInput from './MessageInput';
 import getRequest from './clientAPI';
+import socket from './socket';
 
 function App() {
   
-  const socket = io("https://localhost:3001");
-
+  const { io } = require("socket.io-client"); 
+  const socket = io("http://localhost:3001/");
   //const [socket, setSocket] = useState(null);
   const [resp, setResp] = useState('');
   const [value, setValue] = useState('');
-    useEffect(async() => {
-      const response = await getRequest();
-      const {data} = response;
-      setResp(data);
-      console.log(resp);
-    }, 
+  useEffect(async() => {
+    const response = await getRequest();
+    const {data} = response;
+    setResp(data);
+    console.log(resp);
+  }, [resp]
   )
+
+  // socket.onAny((event, ...args) => { 
+  //   console.log(event, args);});
+  // io.on("connection", (socket) => {
+  //     socket.emit(value != null);
+  //  });
 
   return (
     <div className="Chat">
@@ -29,7 +36,6 @@ function App() {
       <div>
         {socket ? (
           <div className="chat-container">
-            <Messages socket={socket} />
             <MessageInput socket={socket} value={value} setValue={setValue}/>
           </div>
         ) : (
