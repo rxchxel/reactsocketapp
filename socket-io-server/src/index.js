8930e441f0290887b8073dce3e8ccd5f76e3b4b7
bavana <bavana.sachidanand@bjss.com>
2021-11-26 12:34:25 +0000

    added im

--- socket-io-server/src/index.js
+++ socket-io-server/src/index.js
@@ -10,11 +10,10 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
   });
-});
 
 server.listen(3001, () => {
   console.log('listening on *:3001');
