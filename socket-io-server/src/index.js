86d96246f612658bd203ba76db8b5e002c754fe1
rachael.slack <rslack@LPT6368.bjss.co.uk>
2021-11-26 05:08:23 +0000

    added changes

--- socket-io-server/src/index.js
+++ socket-io-server/src/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-
+//const { Server } = require("socket.io");
+const socket = require("socket.io");
+const io = new socket.Server(server);
 const cors = require('cors');
 
 // app.get('/', (req, res) => {
@@ -15,15 +15,17 @@ app.use(cors());
 app.get('/', (req, res) => {
   return res.status(200).send("Hello from the backend")
 });
-app.get('/msgs', (req, res) => {
-  return res.status(200).send("Hello from the backend")
+app.get('/atarichat', (req, res) => {
+  return res.status(200).send("Hello from the backend!!!!!!!!!!!!")
 });
 
-// io.on('connection', (socket) => {
-//     socket.on('chat message', (msg) => {
-//         io.emit('chat message', msg);
-//     });
-//   });
+//io.path("/atarichat");
+
+io.on("connection", (socket) => {
+  socket.emit("message", "did i work?")
+  console.log("i worked this time")
+});
+
 
 server.listen(3001, () => {
   console.log('listening on *:3001');
